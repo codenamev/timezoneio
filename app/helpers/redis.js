f39033458da7572e13d1b4f49bce326ccf7e3c05
Valentino Stoll <vstoll@doximity.com>
2017-07-21 15:01:31 -0400

    Adjust redis helper to use REDIS_URL for prod

--- app/helpers/redis.js
+++ app/helpers/redis.js
@@ -1,8 +1,18 @@
 var redis = require('redis');
+var ENV = require('../../env');
 
-var client = redis.createClient({
-  host: process.env.NODE_ENV === 'production' ? '127.0.0.1' : 'redis',
-});
+var redisConfig = {
+  client: redisClient,
+  ttl: 14 * 86400 // 14 days expiration
+};
+
+if (ENV.NODE_ENV === 'production') {
+  redisConfig.url = ENV.REDIS_URL;
+} else {
+  redisConfig.host = 'redis';
+}
+
+var client = redis.createClient(redisConfig);
 
 // This is a simple wrapper so we can re-use the active redis connection
 
